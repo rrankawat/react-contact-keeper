c151faa3875130f22b89b4da581f3cbec6aeaaec
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2020-05-13 13:17:13 +0530

    Register & Login Components

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import AuthState from './context/auth/AuthState';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 import './App.css';
 
@@ -21,6 +23,8 @@ const App = () => {
               <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
               </Switch>
             </div>
           </Fragment>
