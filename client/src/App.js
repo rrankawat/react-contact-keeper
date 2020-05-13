060bd39da963cbe8b331c3ac2b834a63d98ebb1f
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2020-05-14 01:23:45 +0530

    PrivateRoute Component

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import AuthState from './context/auth/AuthState';
 import AlertState from './context/alert/AlertState';
 
 import setAuthToken from './utils/setAuthToken';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 import Navbar from './components/layout/Navbar';
 import Alerts from './components/layout/Alerts';
@@ -31,7 +32,7 @@ const App = () => {
               <div className="container">
                 <Alerts />
                 <Switch>
-                  <Route exact path="/" component={Home} />
+                  <PrivateRoute exact path="/" component={Home} />
                   <Route exact path="/about" component={About} />
                   <Route exact path="/register" component={Register} />
                   <Route exact path="/login" component={Login} />
