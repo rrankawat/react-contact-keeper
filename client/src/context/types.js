bfeb6724126f21a80308ebd473a5c8d31185b195
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2020-05-13 14:42:27 +0530

    Alert Context

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -10,7 +10,7 @@ export const CLEAR_CURRENT = 'CLEAR_CURRENT';
 
 // Alerts
 export const SET_ALERT = 'SET_ALERT';
-export const REMOVE_CONTACT = 'REMOVE_CONTACT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
 
 // Auth
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
